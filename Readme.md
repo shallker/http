2faea95da5dc1ebee0b44d1720015af1cd2f78ba
Shallker Wang <shallker.wang@profero.com>
2013-09-15 02:20:12 +0800

    First commit

---
+++ Readme.md
@@ -0,0 +1,75 @@
+
+# http
+
+  Ultimate XHR request
+
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install shallker/http
+
+
+## API
+```javascript
+var HTTP = require('http');
+var http = new HTTP;
+```
+
+### http.url(String address)
+
+### http.header(String name, String value)
+
+### http.data(Mix value)
+
+### http.method(String name)
+
+### http.on(String event, Function callback)
+
+### http.off(String event, Function callback)
+
+### http.complete(Function callback)
+
+### http.success(Function callback)
+
+### http.fail(Function callback)
+
+### http.post([String address], [Function callback])
+
+### http.get([String address], [Function callback])
+
+### http.request([String address], [Function callback])
+
+
+## Test
+```javascript
+f(http.url)
+f(http.header)
+f(http.data)
+f(http.method)
+f(http.on)
+f(http.off)
+f(http.complete)
+f(http.success)
+f(http.fail)
+f(http.post)
+f(http.get)
+f(http.request)
+
+ok(http.on('complete', function () { log('complete') }))
+ok(http.on('success', function () { log('success') }))
+ok(http.on('fail', function () { log('fail') }))
+
+ok(http.on(200, function () { log(200) }))
+ok(http.on('Ok', function () { log('Ok') }))
+
+ok(http.on(304, function () { log(304) }))
+ok(http.on('Not Modified', function () { log('Not Modified') }))
+
+ok(http.get('http://lc:3000/test/http.js', function () { log('done') }))
+```
+
+## License
+
+  MIT
---
+++ test/http.js
@@ -0,0 +1,55 @@
+var er = function (m) {throw new Error(m)},
+    ok = function (x) {if (!x) throw new Error(x + ' is not ok'); return 1;},
+    eq = function (x, y) {if (x !== y) er(x + ' not equal ' + y); return 1;},
+    mc = function(ox, oy) {for (var i in ox) {if (!eq(ox[i], oy[i])) er(ox[i] + ' not match ' + oy[i])}}
+    s = function (x) {eq(Object.prototype.toString.call(x), '[object String]')},
+    f = function (x) {eq(Object.prototype.toString.call(x), '[object Function]')},
+    a = function (x) {eq(Object.prototype.toString.call(x), '[object Array]')},
+    b = function (x) {eq(Object.prototype.toString.call(x), '[object Boolean]')},
+    o = function (x) {eq(Object.prototype.toString.call(x), '[object Object]')},
+    log = function () {console.log.apply(console, arguments)};
+
+
+/*
+http
+  .url('')
+  .header(name, value)
+  .data()
+  .method()
+  .on('complete')
+  .on('201')
+  .on(404)
+  .off()
+  .success(callback)
+  .fail(callback)
+  .complete(callback)
+  .post([url], callback)
+  .get([url], callback)
+  .request(callback)
+*/
+
+var HTTP = require('http')
+var http = new HTTP
+
+f(http.url)
+f(http.header)
+f(http.data)
+f(http.method)
+f(http.on)
+f(http.off)
+f(http.success)
+f(http.fail)
+f(http.complete)
+f(http.post)
+f(http.get)
+f(http.request)
+
+ok(http.on('complete', function () {log('complete')}))
+
+ok(http.on(200, function () {log(200)}))
+ok(http.on('Ok', function () {log('Ok')}))
+
+ok(http.on(304, function () {log(304)}))
+ok(http.on('Not Modified', function () {log('Not Modified')}))
+
+ok(http.get('http://lc:3000/test/http.js', function () {log('done')}))
